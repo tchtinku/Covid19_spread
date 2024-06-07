7f58913e0f7150007c4b13e7b87e3e190680a562
tchtinku <tchtinku@gmail.com>
2024-06-08 00:00:19 +0530

    day 195

--- covid19_spread/data/usa/convert.py
+++ covid19_spread/data/usa/convert.py
@@ -38,3 +38,46 @@ def merge_nyc_boroughs(df, ntypes):
     df = df.reset_index()
     print(df[df["region"] == "New York City, New York"])
     return df
+
+def process_time_features(df, pth, shift=0, merge_nyc=False, input_resolution="county"):
+    print(f"Processing {pth} at resolution: {input_resolution}")
+    time_features = pd.read_csv(pth)
+    if input_resolution == "county_state":
+        #Expand state level time features to each county in 'df'
+        idx = df.rename_axis("county").reset_index()[["county"]]
+        idx["region"] = idx["county"].apply(lambda x: x.split(", ")[-1])
+        time_features = time_features.merge(idx, on="region").drop(columns="region")
+        time_features = time_features.rename(columns={"county": "region"})
+    time_features_regions = time_features["region"].unique()
+    ncommon = len(df.index.intersection(time_features_regions))
+    if ncommon != len(df):
+        missing = set(df.index).difference(set(time_features_regions))
+        warnings.warn(
+            f"{pth}: Missing time features for the following reasons: {list(missing)}"
+        )
+    if ncommon != len(time_features_regions):
+        ignoring = set(time_features_regions).difference(set(df.index))
+        warnings.warn(
+            f"{pth}: Ignoring time features for the following resons: {list(ignoring)}"
+        )
+        time_features = time_features[time_features["region"].isin(set(df.index))]
+    if merge_nyc:
+        time_features = merge_nyc_boroughs(
+            time_features, len(time_features["type"].unique())
+        )
+    # Transpose to have two level columns (region, type) and dates as index
+    time_features = time_features.set_index(["region", "type"]).transpose().sort_index()
+    time_features.index = pd.to_datetime(time_features.index)
+    # Trim prefix if it starts before the dates in df
+    time_features = time_features.loc[time_features.index >= df.columns.min()]
+    #Fill in dates that are missing in `time_features` that exists in df
+    time_features = time_features.reindex(df.columns)
+    #shift time features UP by `shift` days
+    time_features = time_features.shift(shift)
+    #forward fill the missing values
+    time_features = time_features.fillna(0)
+    time_features = time_features[time_features.columns.sort_values()]
+    feature_tensors = {
+        region: th.from_numpy(time_features[region].values)
+        for region in time_features.columns.get_level_
+    }
