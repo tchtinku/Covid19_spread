a0c972059feccd84f08d0826a042a1bd576720cf
tchtinku <tchtinku@gmail.com>
2024-06-19 00:16:17 +0530

    day 207: process_mobility.py

--- covid19_spread/data/usa/fb/process_mobility.py
+++ covid19_spread/data/usa/fb/process_mobility.py
@@ -52,5 +52,51 @@ def main():
     df = df.rename(columns={"ds": "date", "polygon_id": "region"})      
     
     df = df.merge(fips_map, left_on="region", right_on="fips")[
-        
+        list(df.columns) + ["location"]
     ]  
+    df = df.drop(columns="region").rename(columns={"location": "region"})
+    
+    def zscore(df):
+        df = (df.values - df.mean(skipna=True)) / df.std(skipna=True)
+        return df
+    
+    def process_df(df, cols):
+        df = df[cols].copy()
+        for (name, _df) in df.groupby("region"):
+            _df = _df.sort_values(by="date")
+            _df = _df.drop_duplicates(subset="date")
+            dates = _df["date"].to_list()
+            assert len(dates) == len(np.unique(dates)), df
+            _df = _df.loc[:, ~_df.columns.duplicated()]
+            _df = _df.drop(columns=["region", "date"]).transpose()
+            _df = _df.rolling(7, min_periods=1, axis=1).mean()
+            #convert relative change to absolute numbers
+            _df.loc["all_day_bing_tiles_visited_relative_change"] += 1
+            _df["region"] = [name] * len(_df)
+            _df.columns = list(map(lambda x: x.strftime(%Y-%m-%d), dates)) +  [
+                "region"
+            ]
+            regions.append(_df.reset_index())
+            
+        df = pd.concat(regions, axis=0, ignore_index=True)
+        cols = ["region"] + [x for x in df.columns if x != "region"]
+        df = df[cols]
+        
+        df = df.rename(columns={"index": "type"})
+        return df
+    
+    county = process_df(df, cols)
+    state = df.copy()
+    state["region"] = state["region"].apply(lambda x: x.split(", ")[-1])
+    state = state.groupby(["region", "date"]).mean.reset_index()
+    state = process_df(state, cols)
+    county = county.fillna(0)
+    state = state.fillna(0)
+    county.round(4).to_csv(f"{SCRIPT_DIR}/mobility_features_county_fb.csv", index=False)
+    state.round(4).to_csv(f"{SCRIPT_DIR}/mobility_features_state_fb.csv", index=False)
+    
+if __name__ == "__main__":
+    main()            
+            
+        
+    
