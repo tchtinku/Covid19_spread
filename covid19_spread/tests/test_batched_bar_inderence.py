40d8c55273d7f391e4df2d83fccab2f5b9fe6714
tchtinku <tchtinku@gmail.com>
2024-07-07 23:55:58 +0530

    day 226: working on tests

--- covid19_spread/tests/test_batched_bar_inderence.py
+++ covid19_spread/tests/test_batched_bar_inderence.py
@@ -16,3 +16,18 @@ class TestBatchedInference:
                     for k, v in cfg["bar"]["train"].items()
                 }
             )
+            opt.fdat = cfg["bar"]["data"]
+            
+            cases, regions, basedate, device = mod.initialize(opt)
+            cases = cases.type(th.get_default_dtype())
+            tmax = cases.size(-1)
+            
+            #torch.bmm can give small precision differences on the CPU when comparing
+            #batched vs. non-batched inputs. If we do too many simulation iterations
+            #this error can compund to highly noticeable values. Limit the number of
+            #iterations to a small value. Interestingly, on the GPU it isn't a problem....
+            sim = mod.func.simulate(tmax, cases, 5, deterministic=True)
+            sim_batched = mod.func.simulate(
+                tmax, cases.repeat(2, 1, 1).contiguous(), 5, deterministic=True
+            )
+            assert (sim - sim_batched[0]).abs().max().item() < 1e-7
---
+++ covid19_spread/tests/test_load.py
@@ -0,0 +1,23 @@
+from covid19_spread import load
+import pandas as pd
+import pytest
+
+DATA_PATH_US_CSV = "covid19_spread/data/usa/data_cases.csv"
+DATA_PATH_NY_CSV = "covid19_spread/data/usa/data_cases_ny.csv"
+
+class TestLoad:
+    @pytest.mark.parametrize("path", [DATA_PATH_US_CSV, DATA_PATH_NY_CSV])
+    def test_load_cases_by_region(self, path):
+        """Confirms cases loaded are per region"""
+        cases_df = load.load_confirmed_by_region(path)
+        assert cases_df.index.name == "date"
+        assert type(cases_df.index.name) == pd.core.indexes.datetimes.DatatimeIndex
+        assert (cases_df >= 0).all().all()
+        
+        regions = cases_df.columns
+        suffolk_present = (
+            "Suffolk County" in regions or "Suffolk County, New York" in regions
+        )
+        assert suffolk_present
+        
+        
